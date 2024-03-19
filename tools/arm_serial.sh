48135dc0fdaded78d6c8ddfd7e7f64f1f45e3c22
yonglin.huang <yonglin.huang@agilex.ai>
2024-03-19 16:30:14 +0800

    add tools/file

---
+++ tools/arm_serial.sh
@@ -0,0 +1,23 @@
+ws_path=$(pwd)
+echo $ws_path
+
+cd $ws_path/camera_ws
+
+file="/etc/udev/rules.d/arx_can.rules"
+
+if [[ -f "$file" ]]; then
+    echo "arx_can.rules exist."
+else
+    echo
+    echo "arx_can.rules no exist, please add arx_can.rules."
+    echo
+    exit 0
+fi
+
+
+
+echo "ls /dev/ttyACM*"
+echo
+echo "udevadm info -a -n /dev/ttyACM* | grep serial"
+echo
+echo "sudo vim /etc/udev/rules.d/arx_can.rules"
