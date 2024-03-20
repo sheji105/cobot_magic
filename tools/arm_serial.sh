d4ebf2ea6eef1b9b9c85fb2ecf4562d34f7f98ac
yonglin.huang <yonglin.huang@agilex.ai>
2024-03-20 09:48:15 +0800

    tools/arm_serial.sh

--- tools/arm_serial.sh
+++ tools/arm_serial.sh
@@ -15,9 +15,10 @@ else
 fi
 
 
-
 echo "ls /dev/ttyACM*"
 echo
 echo "udevadm info -a -n /dev/ttyACM* | grep serial"
 echo
 echo "sudo vim /etc/udev/rules.d/arx_can.rules"
+
+echo "ssh-keygen -f "/home/lin/.ssh/known_hosts" -R "192.168.1.121""
